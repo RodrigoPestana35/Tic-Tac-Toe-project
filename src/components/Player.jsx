00b77af46ec23ff8560678769557b62071f9f790
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-11-26 18:22:10 +0000

    handle user input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react"
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    let [playerName, setPlayerName] = useState(initialName);
     let [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        //setIsEditing(!isEditing);    Não usar dessa forma, pois pode haver problemas de sincronização, pois o valor atualizado pode não ser baseado no valor anterior porque o React agenda as atualizações de estado
+        //setIsEditing(!isEditing);    Não usar dessa forma, pois pode haver problemas de sincronização, 
+        //pois o valor atualizado pode não ser baseado no valor anterior porque o React agenda as atualizações de estado
         setIsEditing((prevIsEditing) => !prevIsEditing);  // Usar dessa forma para garantir que o valor atualizado seja baseado no valor anterior
+    }
+
+    function handleInputChange(event) {
+        setPlayerName(event.target.value)
     }
 
     return(
         <li>
             <span className="player">
-                {isEditing ?  <input type="text" value={name} required/> : <span className="player-name">{name}</span>}
+                {isEditing ?  <input type="text" value={playerName} required onChange={handleInputChange}/> : <span className="player-name">{playerName}</span>}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
