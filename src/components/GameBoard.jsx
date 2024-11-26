ba0106a813bf3abbba57b1e593baaf0d33ebaf0e
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-11-26 20:55:05 +0000

    fix GameBoard cell update logic to ensure proper state mutation

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -13,7 +13,7 @@ export default function GameBoard() {
         setGameBoard((prevGameBoard) => {
             /*prevGameBoard[rowIndex][cellIndex] = "X"; esta forma não é recomendada, pois o React não detecta a mudança
             return prevGameBoard;*/
-            const updatedBoard = { ...prevGameBoard.map(row => [...row]) }; //Cria uma cópia do array, usando o spread operator para copiar cada linha do array
+            const updatedBoard = [ ...prevGameBoard.map(row => [...row])]; //Cria uma cópia do array, usando o spread operator para copiar cada linha do array
             updatedBoard[rowIndex][cellIndex] = "X";  //Atualiza a célula selecionada com o valor "X"
             return updatedBoard; //Retorna o novo estado atualizado com a célula selecionada
         });
