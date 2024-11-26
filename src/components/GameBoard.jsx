c7297fd4c39e4499e985978a90812a9a913c6a85
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-11-26 20:53:34 +0000

    implement game board state management and cell selection functionality

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,13 +7,28 @@ const initialGameBoard = [
 ]
 
 export default function GameBoard() {
-    return(
+    let [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectCell(rowIndex, cellIndex) {
+        setGameBoard((prevGameBoard) => {
+            /*prevGameBoard[rowIndex][cellIndex] = "X"; esta forma não é recomendada, pois o React não detecta a mudança
+            return prevGameBoard;*/
+            const updatedBoard = { ...prevGameBoard.map(row => [...row]) }; //Cria uma cópia do array, usando o spread operator para copiar cada linha do array
+            updatedBoard[rowIndex][cellIndex] = "X";  //Atualiza a célula selecionada com o valor "X"
+            return updatedBoard; //Retorna o novo estado atualizado com a célula selecionada
+        });
+    }
+
+
+    return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <il key={rowIndex}>
                     <ol>
                         {row.map((cell, cellIndex) => (
-                            <li key={cellIndex}><button>{cell}</button></li>
+                            <li key={cellIndex}>
+                                <button onClick={() => handleSelectCell(rowIndex, cellIndex)}>{cell}</button>
+                            </li>
                         ))}
                     </ol>
                 </il>
