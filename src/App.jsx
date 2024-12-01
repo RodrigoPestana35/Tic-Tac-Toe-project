9df4dd612c50aee22e2a20dda0bdcf74f4a2c04d
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-01 00:16:46 +0000

    refactor active player logic and simplify gameTurns handling in App component, without using activePlayer state

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
-  let [activePlayer, setActivePlayer] = useState("X");
+  //let [activePlayer, setActivePlayer] = useState("X");
   let [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectCell(rowIndex, columnIndex){
-    setActivePlayer((prevActivePlayer) => prevActivePlayer === "X" ? "O" : "X");
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  function handleSelectCell(rowIndex, columnIndex) {
+    //setActivePlayer((prevActivePlayer) => prevActivePlayer === "X" ? "O" : "X");
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O';
-      }
-      const updateTurns = [{ square: {row: rowIndex, col: columnIndex}, player: currentPlayer }, ...prevTurns];
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      const updateTurns = [{ square: { row: rowIndex, col: columnIndex }, player: currentPlayer }, ...prevTurns];
       return updateTurns;
     });
   }
@@ -23,12 +30,12 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
         <GameBoard onSelectCell={handleSelectCell} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   )
 }
