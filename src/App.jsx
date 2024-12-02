cc0959acfb887ea07ca0a5b4ded8c5ec379bd1ed
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-02 15:53:47 +0000

    lifting computed values up, lift the gameboard on GameBoard component to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from "react"
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
@@ -16,6 +23,21 @@ function App() {
   let [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+      const {square, player} = turn;
+      const {row, col} = square;
+
+      gameBoard[row][col] = player;
+  }
+
+  for(const combination of WINNING_COMBINATIONS) {
+    const firstSquare = gameBoard[combination[0].row][combination[0].col];
+    const secondSquare = gameBoard[combination[1].row][combination[1].col];
+    const thirdSquare = gameBoard[combination[2].row][combination[2].col];
+  }
 
   function handleSelectCell(rowIndex, columnIndex) {
     //setActivePlayer((prevActivePlayer) => prevActivePlayer === "X" ? "O" : "X");
@@ -33,7 +55,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectCell={handleSelectCell} turns={gameTurns} />
+        <GameBoard onSelectCell={handleSelectCell} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,36 +1,20 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-]
-
-export default function GameBoard({ onSelectCell, turns }) {
-/*     let [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    function handleSelectCell(rowIndex, cellIndex) {
-        setGameBoard((prevGameBoard) => {
-            //prevGameBoard[rowIndex][cellIndex] = "X"; esta forma não é recomendada, pois o React não detecta a mudança
-            //return prevGameBoard;
-            const updatedBoard = [ ...prevGameBoard.map(row => [...row])]; //Cria uma cópia do array, usando o spread operator para copiar cada linha do array
-            updatedBoard[rowIndex][cellIndex] = activePlayerSymbol;  //Atualiza a célula selecionada com o símbolo do jogador ativo
-            return updatedBoard; //Retorna o novo estado atualizado com a célula selecionada
-        });
-        onSelectCell();
-    } */
-
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-        const {square, player} = turn;
-        const {row, col} = square;
-
-        gameBoard[row][col] = player;
-    }
-
+export default function GameBoard({ onSelectCell, board }) {
+    /*     let [gameBoard, setGameBoard] = useState(initialGameBoard);
+    
+        function handleSelectCell(rowIndex, cellIndex) {
+            setGameBoard((prevGameBoard) => {
+                //prevGameBoard[rowIndex][cellIndex] = "X"; esta forma não é recomendada, pois o React não detecta a mudança
+                //return prevGameBoard;
+                const updatedBoard = [ ...prevGameBoard.map(row => [...row])]; //Cria uma cópia do array, usando o spread operator para copiar cada linha do array
+                updatedBoard[rowIndex][cellIndex] = activePlayerSymbol;  //Atualiza a célula selecionada com o símbolo do jogador ativo
+                return updatedBoard; //Retorna o novo estado atualizado com a célula selecionada
+            });
+            onSelectCell();
+        } */
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <il key={rowIndex}>
                     <ol>
                         {row.map((cell, cellIndex) => (
